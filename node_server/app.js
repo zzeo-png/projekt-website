4f3aba3278e63a906da44b8a68fccbfab11a7981
zzeo-png <red.builder.h2o@gmail.com>
2023-05-26 18:19:22 +0200

    added MYSQL and CORS

---
+++ node_server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+
+const port = 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json('Hello world!')
+})
+
+app.post('/test', (req, res) => {
+    const test = req.body.test
+
+    console.log(test)
+    res.json(test)
+})
+
+app.listen(port, ()=> {
+    console.log(`Node running on port ${port}`)
+})
